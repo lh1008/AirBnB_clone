011a801daeee77269a5baf1dc8344126d95a04d6
MarcoANT9 <acalde27@hotmail.com>
2020-02-17 21:26:25 +0000

    New commit, file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,8 @@
 """ This is the base class for the AirBnB clone """
 
 import datetime
-import json
 import uuid
+from models import storage
 
 
 class BaseModel():
@@ -19,6 +19,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
         else:
             for key in kwargs:
@@ -47,13 +48,12 @@ class BaseModel():
         """ Updates the public instance attribute "update_at" with the current
             datetime.                                                       """
         updated = datetime.datetime.now()
-        self.updated_at = updated
+        storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__ of the
             instance.                                                       """
         diction = {}
-
         diction['__class__'] = self.__class__.__name__
 
         if self.__dict__:
